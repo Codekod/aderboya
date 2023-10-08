2d81af56cdf9420878d33a4709db78d3d4d543ca
Codekod <meliheken@aof.anadolu.edu.tr>
2023-10-08 10:46:47 +0300

    Team Section Added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,39 @@
-const logo = document.querySelector(".logo");
+const logo = document.querySelector(".logo img");
 
 window.addEventListener("scroll", () => {
     const services = document.getElementById("services");
-    const servicesTop = services.offsetTop;
-    const scrollTop = window.scrollY;
+    const portfolio = document.getElementById("portfolio");
+    const team = document.getElementById("team");
+    const blog = document.getElementById("blog");
+    const contact = document.getElementById("contact");
 
-    if (scrollTop >= servicesTop) {
-        // Scroll, "services" bölümünün üzerine geldiğinde logo değişikliğini yapın
-        logo.src = "./img/white_logo_transparent_background.png";
-    } else {
-        // Scroll, "services" bölümünün üzerinden ayrıldığında orijinal logo'ya geri dönün
+    const servicesTop = services.offsetTop;
+    const servicesBottom = services.offsetTop + services.offsetHeight;
+
+    const portfolioTop = portfolio.offsetTop;
+    const portfolioBottom = portfolio.offsetTop + portfolio.offsetHeight;
+
+    const teamTop = team.offsetTop;
+    const teamBottom = team.offsetTop + team.offsetHeight;
+
+    const blogTop = blog.offsetTop;
+    const blogBottom = blog.offsetTop + blog.offsetHeight;
+
+    const contactTop = contact.offsetTop;
+
+    const scrollTop = window.scrollY + window.innerHeight;
+
+    if (scrollTop < servicesTop) {
         logo.src = "./img/logo_transparent_background.png";
+    } else if (scrollTop >= servicesTop && scrollTop <= servicesBottom) {
+        logo.src = "./img/white_logo_transparent_background.png";
+    } else if (scrollTop > servicesBottom && scrollTop < blogTop) {
+        logo.src = "./img/logo_transparent_background.png";
+    } else if (scrollTop >= blogTop && scrollTop <= blogBottom) {
+        logo.src = "./img/white_logo_transparent_background.png";
+    } else if (scrollTop > blogBottom && scrollTop < contactTop) {
+        logo.src = "./img/logo_transparent_background.png";
+    } else {
+        logo.src = "./img/white_logo_transparent_background.png";
     }
 });
